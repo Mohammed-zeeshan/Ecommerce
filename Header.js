dd59554571f078436397abf928e7bef08b69f580
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-26 02:55:30 +0530

    Cart Component

--- Header.js
+++ Header.js
@@ -1,16 +1,79 @@
-import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import CloseButton from 'react-bootstrap/CloseButton';
 
 const Header = () => {
+    const cartElements = [
+
+        {
+        
+        title: 'Colors',
+        
+        price: 100,
+        
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+        
+        quantity: 2,
+        
+        },
+        
+        {
+        
+        title: 'Black and white Colors',
+        
+        price: 50,
+        
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+        
+        quantity: 3,
+        
+        },
+        
+        {
+        
+        title: 'Yellow and Black Colors',
+        
+        price: 70,
+        
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+        
+        quantity: 1,
+        
+        }
+        
+    ]
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleHide = () => setShow(false);
   return (
-    <header>
-        <Nav className="justify-content-center">
+    <>
+        <Nav expand="lg" className="justify-content-center bg-body-tertiary">
             <Button variant="light">HOME</Button>{' '}
             <Button variant="light">STORE</Button>{' '}
             <Button variant="light">ABOUT</Button>{' '}
-            <Button variant='outline-primary'>cart</Button>{' '}
+            <Button variant='outline-primary' onClick={handleShow}>cart</Button>
         </Nav>
-    </header>
+        <Modal show={show} onHide={handleHide}>
+            <Modal.Header>
+            <Nav expand="lg" className="justify-content-center"><h1>CART</h1></Nav>
+            <CloseButton onClick={handleHide}/>
+            </Modal.Header>
+            <Modal.Body>
+            <label>ITEM</label>
+            <label>PRICE</label>
+            <label>QUANTITY</label>
+            {cartElements.map(item => (
+                <div><Image src={item.imageUrl} rounded /> {item.title}  {item.price}  {item.quantity} <Button variant="primary">Remove</Button> </div>
+            ))}
+            </Modal.Body>
+            <Modal.Footer>
+                <Nav expand="lg" className='justify-content-center'><Button variant='primary'>PURCHASE</Button></Nav>
+            </Modal.Footer>
+        </Modal>
+    </>
   )
 }
 
