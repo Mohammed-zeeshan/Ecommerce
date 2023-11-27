8306d81639ded9933b7f63ab215ccc753794de04
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-27 22:57:20 +0530

    Implement the Products Page

--- App.js
+++ App.js
@@ -1,16 +1,33 @@
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import List from "./Components/List";
-import Title from "./Components/Title";
+import About from "./Components/About";
+import Store from "./Components/Store";
+import Homepage from "./Components/Homepage";
+import ContactUs from "./Components/ContactUs";
+import ProductPage from "./Components/ProductPage";
+import { Redirect, Route } from "react-router-dom/cjs/react-router-dom.min";
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <Title />
-      <List />
-      <Footer />
-    </div>
+      <Route path='/' exact>
+        <Redirect to='/Store' />
+      </Route>
+      <Route path="/Store" >
+        <Store />
+      </Route>
+      <Route path="/About" >
+        <About />
+      </Route>
+      <Route path="/Homepage" >
+        <Homepage />
+      </Route>
+      <Route path="/ContactUs" >
+        <ContactUs />
+      </Route>
+      <Route path="/ProductPage/:productId" >
+        <ProductPage />
+      </Route>
+    </div> 
   );
 }
 
